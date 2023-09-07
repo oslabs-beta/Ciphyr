0213704228d84c23443bc92855a49d42d09031f2
John Le <johnvle00@gmaile.com>
2023-09-07 16:28:55 -0700

    initial commit

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,8 +4,9 @@ export default function Homepage() {
 
   return (
     <>
-     <div>API Keys</div>
-     
+    <div className="bg-blue-500 p-4">
+      <h1 className="text-2xl font-semibold text-white">Hello, Tailwind CSS!</h1>
+    </div>
     </>
   )
 }
