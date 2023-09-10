85bd9de5de9a2b03b41456986ee85a2eb32a4fe7
John Le <johnvle00@gmaile.com>
2023-09-10 11:25:34 -0700

    [RESOLVED] merge with aidan

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import HomePage from './components/HomePage.jsx/';
-import Dashboard from './components/Dashboard.jsx';
-import Login from './components/Login.jsx';
-import Signup from './components/Signup.jsx'
-import Navbar from './components/Navbar'
+import HomePage from "./components/HomePage.jsx/";
+import Dashboard from "./components/Dashboard.jsx";
+import Login from "./components/Login.jsx";
+import Signup from "./components/Signup.jsx";
 
 function App() {
-
   return (
     <div>
-      <Navbar />
       <BrowserRouter>
         <Routes>
-          <Route path = '/' element={<HomePage />}></Route>
-          <Route path = '/dashboard' element={<Dashboard />}></Route>
-          <Route path = '/login' element={<Login />}></Route>
-          <Route path = '/signup' element={<Signup />}></Route>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
