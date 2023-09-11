96e4b2c05e0e76e11d7299fc9fb7a75f66677521
John Le <johnvle00@gmaile.com>
2023-09-11 13:44:50 -0700

    [TanStack v0.7] ALL OF THE LIGHTS

--- server/routes/logRouter.js
+++ server/routes/logRouter.js
@@ -5,6 +5,8 @@ const logController = require('../controllers/logController');
 
 router.get('/', logController.getAllLog, async (req, res) => {
   return res.status(200).json(res.locals.allLog);
+  // .writeHead(200, { 'Content-Type': 'application/javascript' })
+  // .end(`window.__APP_VARIABLES__ = ${JSON.stringify(res.locals.allLog)}`);
 });
 
 module.exports = router;
--- src/components/dashboard/DashTable.jsx
+++ src/components/dashboard/DashTable.jsx
@@ -4,13 +4,18 @@ import {
   flexRender,
   getCoreRowModel,
   useReactTable,
+  getPaginationRowModel,
+  getFilteredRowModel,
 } from "@tanstack/react-table";
 import mData from "./MOCK_DATA.json";
+import FilterFunction from "./FilterFunction";
 
 export default function DashTable() {
+  const [filtering, setFiltering] = useState("");
+  const [columnFilters, setColumnFilters] = useState("");
   // const [tableRows, setTableRows] = useState([]);
 
-  //One time on load of page render the instances logs
+  // //One time on load of page render the instances logs
   // useEffect(() => {
   //   fetchLogs();
   // }, []);
@@ -69,10 +74,24 @@ export default function DashTable() {
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      globalFilter: filtering,
+      columnFilters: columnFilters,
+    },
+    onGlobalFilterChanged: setFiltering,
+    onColumnFiltersChange: setColumnFilters,
   });
 
   return (
     <div className="w-full">
+      <input
+        type="text"
+        placeholder="Search by keywords"
+        value={filtering}
+        onChange={(e) => setFiltering(e.target.value)}
+      />
       <table className="w-full">
         <thead>
           {table.getHeaderGroups().map((headerGroup) => (
@@ -82,9 +101,16 @@ export default function DashTable() {
                   key={header.id}
                   className="py-2 pr-12 text-left border-b border-gray-200 bg-gray-100"
                 >
-                  {flexRender(
-                    header.column.columnDef.header,
-                    header.getContext()
+                  {header.isPlaceholder ? null : (
+                    <>
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                      {/* <div>
+                        <FilterFunction column={header.column} table={table} />
+                      </div> */}
+                    </>
                   )}
                 </th>
               ))}
@@ -106,6 +132,135 @@ export default function DashTable() {
           ))}
         </tbody>
       </table>
+      <div>
+        <button
+          className="border border-slate-300 mr-2 px-2 py-1 rounded"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {"<"}
+        </button>
+        <button
+          className="border border-slate-300 mr-2 px-2 py-1 rounded"
+          onClick={() => table.setPageIndex((oldPageIndex) => oldPageIndex - 1)}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {table.getState().pagination.pageIndex - 1}
+        </button>
+        <button
+          className="border border-slate-300 mr-2 px-2 py-1 rounded"
+          onClick={() => table.setPageIndex()}
+        >
+          {table.getState().pagination.pageIndex}
+        </button>
+        <button
+          className="border border-slate-300 mr-2 px-2 py-1 rounded"
+          onClick={() => table.setPageIndex((oldPageIndex) => oldPageIndex + 1)}
+          disabled={!table.getCanNextPage()}
+        >
+          {table.getState().pagination.pageIndex + 1}
+        </button>
+        <button
+          className="border border-slate-300 mr-2 px-2 py-1 rounded"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {">"}
+        </button>
+      </div>
     </div>
   );
 }
+
+// function Filter({ column, table }) {
+//   const firstValue = table.getPreFilteredRowModel().flatRows[0]?.getValue(column.id);
+
+//   const columnFilterValue = column.getFilterValue();
+
+//   const sortedUniqueValues = React.useMemo(
+//     () =>
+//       typeof firstValue === 'number'
+//         ? []
+//         : Array.from(column.getFacetedUniqueValues().keys()).sort(),
+//     [column.getFacetedUniqueValues()]
+//   );
+
+//   return typeof firstValue === 'number' ? (
+//     <div>
+//       <div className="flex space-x-2">
+//         <DebouncedInput
+//           type="number"
+//           min={Number(column.getFacetedMinMaxValues()?.[0] ?? '')}
+//           max={Number(column.getFacetedMinMaxValues()?.[1] ?? '')}
+//           value={(columnFilterValue?.[0] ?? '')}
+//           onChange={(value) =>
+//             column.setFilterValue((old) => [value, old?.[1]])
+//           }
+//           placeholder={`Min ${
+//             column.getFacetedMinMaxValues()?.[0]
+//               ? `(${column.getFacetedMinMaxValues()?.[0]})`
+//               : ''
+//           }`}
+//           className="w-24 border shadow rounded"
+//         />
+//         <DebouncedInput
+//           type="number"
+//           min={Number(column.getFacetedMinMaxValues()?.[0] ?? '')}
+//           max={Number(column.getFacetedMinMaxValues()?.[1] ?? '')}
+//           value={(columnFilterValue?.[1] ?? '')}
+//           onChange={(value) =>
+//             column.setFilterValue((old) => [old?.[0], value])
+//           }
+//           placeholder={`Max ${
+//             column.getFacetedMinMaxValues()?.[1]
+//               ? `(${column.getFacetedMinMaxValues()?.[1]})`
+//               : ''
+//           }`}
+//           className="w-24 border shadow rounded"
+//         />
+//       </div>
+//       <div className="h-1" />
+//     </div>
+//   ) : (
+//     <>
+//       <datalist id={column.id + 'list'}>
+//         {sortedUniqueValues.slice(0, 5000).map((value) => (
+//           <option value={value} key={value} />
+//         ))}
+//       </datalist>
+//       <DebouncedInput
+//         type="text"
+//         value={(columnFilterValue ?? '')}
+//         onChange={(value) => column.setFilterValue(value)}
+//         placeholder={`Search... (${column.getFacetedUniqueValues().size})`}
+//         className="w-36 border shadow rounded"
+//         list={column.id + 'list'}
+//       />
+//       <div className="h-1" />
+//     </>
+//   );
+// }
+
+// // A debounced input react component
+// function DebouncedInput({
+//   value: initialValue,
+//   onChange,
+//   debounce = 500,
+//   ...props
+// }) {
+//   const [value, setValue] = React.useState(initialValue);
+
+//   React.useEffect(() => {
+//     setValue(initialValue);
+//   }, [initialValue]);
+
+//   React.useEffect(() => {
+//     const timeout = setTimeout(() => {
+//       onChange(value);
+//     }, debounce);
+
+//     return () => clearTimeout(timeout);
+//   }, [value]);
+
+//   return <input {...props} value={value} onChange={(e) => setValue(e.target.value)} />;
+// }
