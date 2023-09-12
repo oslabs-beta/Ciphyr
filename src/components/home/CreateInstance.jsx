ec0ba24d802ac02922f24dc896bb744acbe2ed0b
Aidan <ajberger905@gmail.com>
2023-09-11 20:45:44 -0400

    Instance modal set up

--- src/components/home/CreateInstance.jsx
+++ src/components/home/CreateInstance.jsx
@@ -1,10 +1,10 @@
-export default function Dashboard() {
+export default function Dashboard(props) {
   // label for instance table
   // will receive api key from db
 
   return (
     <div className="flex justify-center bg-gray-200 hover:bg-gray-300 rounded mt-5 w-15rem py-2 px-4">
-      <button className="">Create new instance</button>
+      <button onClick={() => props.toggleModal()} className="">Create new instance</button>
     </div>
   );
 }
