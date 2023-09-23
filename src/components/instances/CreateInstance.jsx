0915b08e3d94d201cb4fb2675e3185fcedeeacd5
johnvle <johnvle00@gmail.com>
2023-09-23 15:06:10 -0700

    [FEAT] added description to create new instance button

--- src/components/instances/CreateInstance.jsx
+++ src/components/instances/CreateInstance.jsx
@@ -5,7 +5,7 @@ export default function Dashboard(props) {
   return (
     <div className="flex justify-center border border-slate-400 hover:bg-white hover:border-primary hover:text-primary font:semi-bold rounded mt-4 w-15rem py-2 px-4 mx-4 text-slate-800">
       <button onClick={() => props.toggleModal()} className="">
-
+        Create new instance
       </button>
     </div>
   );
