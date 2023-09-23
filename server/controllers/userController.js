0db1ce4c1f61fc9181fe20e671363268942577ee
johnvle <johnvle00@gmail.com>
2023-09-23 13:52:28 -0700

    [feat] completedh home page intro and time

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,18 +1,22 @@
-const bcrypt = require('bcrypt');
-const db = require('../db');
+const bcrypt = require("bcrypt");
+const db = require("../db");
 const salt = 10;
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const userController = {};
 
 userController.signup = async (req, res, next) => {
   try {
     const { username, password, email } = req.body;
-    console.log('USERNAME', username)
-    console.log('password', password)
-    console.log('email', email)
-    if (username === undefined || password === undefined || email === undefined) {
-      res.locals.newClient = { message: 'All input fields required'};
+    console.log("USERNAME", username);
+    console.log("password", password);
+    console.log("email", email);
+    if (
+      username === undefined ||
+      password === undefined ||
+      email === undefined
+    ) {
+      res.locals.newClient = { message: "All input fields required" };
       return next();
     }
     let hashedPassword = await bcrypt.hash(password, salt);
@@ -24,21 +28,20 @@ userController.signup = async (req, res, next) => {
     const emailResult = await db.query(emailQuery);
 
     if (emailResult.row) {
-      res.locals.newClient = { message: 'Email already in use' };
+      res.locals.newClient = { message: "Email already in use" };
     } else if (usernameResult.row) {
-      res.locals.newClient = { message: 'Username already exist' };
+      res.locals.newClient = { message: "Username already exist" };
     } else {
-      const createQuery = `INSERT INTO clients ( username, password, email) VALUES ('${username}','${hashedPassword}','${email}');`
+      const createQuery = `INSERT INTO clients ( username, password, email) VALUES ('${username}','${hashedPassword}','${email}');`;
       const create = await db.query(createQuery);
-      console.log('creating new client', create);
-      res.locals.newClient = { message: 'Client created' };
+      console.log("creating new client", create);
+      res.locals.newClient = { message: "Client created" };
     }
     return next();
   } catch (err) {
     return next(err);
   }
 };
-
 
 userController.login = async (req, res, next) => {
   try {
@@ -48,17 +51,43 @@ userController.login = async (req, res, next) => {
     const passwordResult = await db.query(passwordQuery);
     const clientID = passwordResult.rows[0].client_id;
 
-    const verified = await bcrypt.compare(password, passwordResult.rows[0].password);
+    const verified = await bcrypt.compare(
+      password,
+      passwordResult.rows[0].password
+    );
 
     if (!verified) {
-      res.locals.result = {verified: verified, message: "Incorrect information entered"}
+      res.locals.result = {
+        verified: verified,
+        message: "Incorrect information entered",
+      };
     } else {
       // After successful login, store client id in httpOnly cookie to prevent access from DOM action
-      const jwtToken = jwt.sign({client_id: clientID}, process.env.TOKEN_SECRET);
-      res.cookie('token', jwtToken, { httpOnly: true, secure: true });
-      res.locals.result = {verified: verified, message: "login successfully"}
+      const jwtToken = jwt.sign(
+        { client_id: clientID },
+        process.env.TOKEN_SECRET
+      );
+      res.cookie("token", jwtToken, { httpOnly: true, secure: true });
+      res.locals.result = { verified: verified, message: "login successfully" };
     }
     return next();
+  } catch (err) {
+    return next(err);
+  }
+};
+
+userController.getUserInfo = async (req, res, next) => {
+  try {
+    const jwtToken = req.cookies.token;
+    // retrieve instances based the clien_id in JWT token
+    jwt.verify(jwtToken, process.env.TOKEN_SECRET, async (err, user) => {
+      if (err) return res.sendStatus(403);
+
+      const userQuery = `SELECT username FROM clients WHERE client_id = '${user.client_id}';`;
+      const userResult = await db.query(userQuery);
+      res.locals.userInfo = userResult.rows[0].username;
+      return next();
+    });
   } catch (err) {
     return next(err);
   }
