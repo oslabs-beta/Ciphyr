7ee91a1c630491b79b9f3484db8e7a187394c5f7
Michael Zhang <mjzhang95@gmail.com>
2023-09-25 10:35:04 -0400

    still testing oauth

--- server/routes/oauthRouter.js
+++ server/routes/oauthRouter.js
@@ -8,31 +8,31 @@ const router = express.Router();
 const clientID = process.env.GitHubClientID ?? "";
 const clientSecret = process.env.GitHubClientSecret ?? "";
 
-// passport.use(
-//   new githubStrategy(
-//     {
-//       clientID: clientID,
-//       clientSecret: clientSecret,
-//       callbackURL: 'http://localhost:5173/home',
-//     },
-//     async (accessToken, refreshToken, profile, cb) => {
-//       try {
-//         const userQuery = `SELECT * FROM clients WHERE username = $1`;
-//         const { rows } = await db.query(userQuery, [profile.username]);
+passport.use(
+  new githubStrategy(
+    {
+      clientID: clientID,
+      clientSecret: clientSecret,
+      callbackURL: 'http://localhost:5173/home',
+    },
+    async (accessToken, refreshToken, profile, cb) => {
+      try {
+        const userQuery = `SELECT * FROM clients WHERE username = $1`;
+        const { rows } = await db.query(userQuery, [profile.username]);
 
-//         if (rows.length === 0) {
-//           const gitHubUser = `INSERT INTO clients (username, password, email) VALUES ($1, $2, $3)`;
-//           const values = [profile.username, 'ghubpassword', 'ghub@ghub.com'];
-//           const createdUser = await db.query(gitHubUser, values);
-//         }
+        if (rows.length === 0) {
+          const gitHubUser = `INSERT INTO clients (username, password, email) VALUES ($1, $2, $3)`;
+          const values = [profile.username, 'ghubpassword', 'ghub@ghub.com'];
+          const createdUser = await db.query(gitHubUser, values);
+        }
 
-//         return cb(null, profile);
-//       } catch (error) {
-//         return cb(error);
-//       }
-//     }
-//   )
-// );
+        return cb(null, profile);
+      } catch (error) {
+        return cb(error);
+      }
+    }
+  )
+);
 
 
 router.get('/', passport.authenticate('github', { scope: ['user:email'] }));
