106d84f50a0db4a0d76014ad4ea907df894f3bf3
JeremyHcWang <haocheng.9758@gmail.com>
2023-09-25 11:12:06 -0400

    update library

--- CiphyrLibrary/index.js
+++ CiphyrLibrary/index.js
@@ -1,107 +1,138 @@
-import gql from "graphql-tag";
-import db from "./PSQL.js";
-import dotenv from "dotenv";
+import gql from 'graphql-tag';
+import { GraphQLError } from 'graphql';
+import dotenv from 'dotenv';
+//import jwt from 'jsonwebtoken';
+import pkg from 'pg';
+
 dotenv.config();
+
+const { Pool } = pkg;
+
+const PG_URI = process.env.SQL_DB;
+
+const pool = new Pool({
+  connectionString: PG_URI,
+});
+
+const db = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback);
+  },
+};
 
 const ciphyr = {};
 
-// Plugin methods
-ciphyr.myPlugin = {
-  async serverWillStart() {
-    try {
-      console.log("Ciphyr starting up!");
-      // Additional logic related to server startup can go here
-    } catch (error) {
-      throw new Error("Error during server startup: " + error.message);
-    }
-  },
-
-  async requestDidStart(context) {
-    try {
-      // Call the function to set start time
-      ciphyr.getStartTime();
-      // Return an object containing the willSendResponse function
-      return {
-        async willSendResponse(requestContext) {
-          try {
-            // Call the function to convert the response
-            ciphyr.convertStr(requestContext);
-          } catch (err) {
-            throw new Error("Conversion failed: " + err.message);
-          }
-        },
-      };
-    } catch (err) {
-      throw new Error("Request did not start: " + err.message);
-    }
-  },
-};
+// ciphyr.authError = new GraphQLError('You are not authorized.', {
+//   extensions: {
+//     code: 'Unauthorized',
+//   },
+// });
 
 ciphyr.getStartTime = () => {
   ciphyr.startTime = Date.now();
-};
+}
+
+// getAuthInfo need more test
+
+// ciphyr.getAuthInfo = (obj) => {
+//   const token = obj.contextValue.token
+//   // case: no Auth
+//   if (token === '') {
+//     return '';
+//   }
+
+//   // case: JWT
+//   if (true) {
+//     return jwt.verify(token, process.env.JWT_SECRET)
+//   }
+// }
 
 ciphyr.convertStr = async (query) => {
-  const nested = (str) => {
+
+  const getDepth = (str) => {
     let max = 0;
     let count = 0;
 
-    for (let i = 0; i < str.length; i++) {
-      if (str[i] === "{") {
-        count++;
-        if (count > max) {
-          max = count;
+    for(let i = 0; i < str.length; i++) {
+        if (str[i] === '{') {
+            count++;
+            if (count > max) {
+                max = count;
+            }
+        } else if (str[i] === '}') {
+            count--;
         }
-      } else if (str[i] === "}") {
-        count--;
-      }
     }
 
-    return max - 1; // Subtract 1 because the outermost brackets should not be considered in the count
-  };
+    return max - 1;  // Subtract 1 because the outermost brackets should not be considered in the count
+  }
 
   const queryString = query.request.query;
   // Parse the GraphQL query string into an AST
   const queryAST = gql(queryString);
   // Convert the AST to a JavaScript object
   const queryObject = JSON.parse(JSON.stringify(queryAST));
-  const definitions = queryObject.definitions;
+  const definitions = queryObject.definitions; 
 
-  const result = {};
+  const result = {};  
   //type of query
   result.operation = definitions[0].operation;
-  //name of query
-  result.queryName = definitions[0].name.value;
+  //name of query (check if name is provided)
+  result.queryName = (definitions[0].name === undefined) ? '' : definitions[0].name.value;
   //query string
-  result.queryString = queryString
-    .replace(/ /g, "")
-    .replace(/\s+/g, "")
-    .replace(`${result.operation}`, "")
-    .replace(`${result.queryName}`, "");
-  //query string structure
+  result.queryString = queryString.replace(/ /g, '').replace(/\s+/g, '')
+    .replace(`${result.operation}`, '').replace(`${result.queryName}`,'');
+  //query raw string
   result.raw = queryString;
   //depth of query
-  result.depth = nested(queryString);
-  //latency of query
+  result.depth = getDepth(queryString);
+  // if error occured
+  if (query.response.body.singleResult.errors === undefined) {
+    result.error_occured = false;
+    result.error_code = '';
+  } else {
+    result.error_occured = true;
+    result.error_code = query.response.body.singleResult.errors[0].extensions.code
+  }
+  //latency of query 
   result.latency = Date.now() - ciphyr.startTime;
 
-  console.log("result", result);
+  console.log('result', result);
 
-  ciphyr.savingQuery(result);
-};
+  //ciphyr.savingQuery(result);
+}
 
+//save incoming query into PostgresQL
 ciphyr.savingQuery = async (result) => {
-  // will the user be willing to send query log to Ciphyr's database?
-  // how to connect to user's own database instead
-  const sqlQuery = `INSERT INTO log (operation, query_name, log, raw, depth, latency, api_key) 
+  const sqlQuery = `INSERT INTO log (operation, query_name, log, raw, depth, 
+    latency, api_key, error_occured, error_code) 
     VALUES ('${result.operation}', '${result.queryName}', 
-      '${result.queryString}', '${result.raw}', '${result.depth}', '${result.latency}', '${process.env.API_KEY}');`;
+      '${result.queryString}', '${result.raw}', '${result.depth}', '${result.latency}', 
+      '${process.env.API_KEY}', '${result.error_occured}', '${result.error_code}');`
   try {
     const output = await db.query(sqlQuery);
     console.log(output);
-  } catch (err) {
+  } catch(err) {
     console.log(err);
   }
+}
+
+ciphyr.myPlugin = {
+  async serverWillStart() {
+    console.log('Server starting up!');
+  },
+  // ciphyr.convertStr is used in this event to sanitize query logs and send them to DB
+  async requestDidStart(context) {
+    console.log('In requestDidStart');
+    ciphyr.getStartTime();
+    return {
+      async willSendResponse(requestContext) {
+        console.log('In willSendResponse')
+        ciphyr.convertStr(requestContext);
+      }
+    }
+  }
+
 };
 
-module.exports = ciphyr;
+export default ciphyr;
