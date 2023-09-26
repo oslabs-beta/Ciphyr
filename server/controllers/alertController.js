6082c308e45420cbbb37e3a35ea7878cdf4fbd58
Aidan <ajberger905@gmail.com>
2023-09-26 18:15:03 -0400

    getCriteria middleware functioning

--- server/controllers/alertController.js
+++ server/controllers/alertController.js
@@ -5,13 +5,12 @@ const alertController = {}
 alertController.getCriteria = async (req, res, next) => {
   console.log('in getCriteria');
   const username = 'JASON2'
-  const query = `SELECT depth_preference FROM clients WHERE username = ${username}`
+  const query = `SELECT depth_preference FROM clients WHERE username = '${username}'`
   try {
     console.log('in try block of get criteria')
     const result = await db.query(query);
-    const depthPreference = await result.json();
-    res.locals.preference = depthPreference;
-    next()
+    res.locals.preference = result.rows[0].depth_preference;
+    return next()
   } catch (err) {
     console.log(err)
   }
