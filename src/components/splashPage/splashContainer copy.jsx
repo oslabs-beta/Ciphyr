610f9ae592f1b44e916dd30dd15bf5af66748c30
johnvle <johnvle00@gmail.com>
2023-09-25 20:29:00 -0700

    FEAT finally finished the html for main portion of the  splash

--- src/components/splashPage/splashContainer copy.jsx
+++ src/components/splashPage/splashContainer copy.jsx
@@ -105,10 +105,36 @@ export default function splashContainer() {
           </div>
         </section>
         <section className="flex flex-col w-full items-center border border-sky-800 ">
-            <div className="flex flex-row w-full py-16 justify-center">
-              <div className="ml-16 text-3xl font-light">Getting started</div>
+          <div className="flex flex-row w-full py-16 justify-center">
+            <div className="ml-16 text-3xl font-light">Getting started</div>
+          </div>
+        </section>
+        <section className="flex flex-col w-full items-center border border-sky-800 ">
+          <div className="flex flex-row  border-emerald-400 w-full my-4 py-6">
+            <aside className="w-1/2 flex justify-center border-2 border-dashed mx-2 my-2">
+              <img src={ciphyrLogo} className="w-48" />
+            </aside>
+            <div className="w-1/2  border-dashed mx-2 my-2 flex justify-center items-center">
+              <div className="text-xl">NPM install Ciphyr</div>
             </div>
-          </section>
+          </div>
+          <div className="flex flex-row  border-emerald-400 w-full mx-4 my-4 py-6">
+            <div className="w-1/2 border-dashed mx-2 my-2 flex justify-center items-center">
+              <div className="text-xl">Plug in config</div>
+            </div>
+            <aside className="w-1/2 flex justify-center border-2 border-dashed mx-2 my-2">
+              <img src={ciphyrLogo} className="w-48" />
+            </aside>
+          </div>
+          <div className="flex flex-row  border-emerald-400 w-full mx-4 my-4 py-6">
+            <aside className="w-1/2 flex justify-center border-2 border-dashed mx-2 my-2">
+              <img src={ciphyrLogo} className="w-48" />
+            </aside>
+            <div className="w-1/2  border-dashed mx-2 my-2 flex justify-center items-center">
+              <a className="text-xl">Create an account</a>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   );
