b83ae4e09c0258383b465b42f223e08a8d1d4901
Michael Zhang <mjzhang95@gmail.com>
2023-09-28 11:16:31 -0400

    testing new proxy viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,24 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
     globals: true,
   },
-  plugins: [
-    react()],
+  plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:3000",
+      '/api': {
+        target: 'https://mikeciphyr-c90a2b03292c.herokuapp.com/',
         changeOrigin: true,
         secure: false,
       },
     },
   },
   build: {
-    outDir: "dist",
+    outDir: 'dist',
     sourcemap: true,
   },
 });
